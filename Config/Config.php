66a5ac45c39d3b1bae24f4a697154a777106502f
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-22 23:25:26 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -5,11 +5,4 @@
     const PASS = "6VILXUa95VDiqCGWTpom";
     const CHARSET = "charset=utf8";
 
-
-  $bdd = nueva PDO(
-    	"mysql:host=". getenv("bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com") . ";nombrebd=" . getenv("bpcek3ew2gqv7rdmv8nj"),
-    	getenv("useylzzrcbw6nak3"),
-    	getenv("6VILXUa95VDiqCGWTpom")
-  );
-
 ?>
