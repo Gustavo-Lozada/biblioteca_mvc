91821dec0504cc87f0e71641bc379d9ae628b5c1
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-22 23:13:14 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,26 @@
 <?php
-    
-    const HOST = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
-    const BD = "bpcek3ew2gqv7rdmv8nj";
-    const DB_USER = "useylzzrcbw6nak3";
-    const PASS = "6VILXUa95VDiqCGWTpom";
-    const CHARSET = "charset=utf8";
+
+private $hostname = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
+private $database = "bpcek3ew2gqv7rdmv8nj";
+private $username = "useylzzrcbw6nak3";
+private $password = "6VILXUa95VDiqCGWTpom";
+private $charset = "utf8";
+
+function conectar()
+    {
+        try{
+            $conexion = "mysql:host=" . $this->hostname . "; dbname=" . $this->database . "; charset=" . $this->charset;
+            $options = [
+                PDD::ATTR_ERMODE => PDD::ERRMODE_EXCEPTION,
+                PDD::ATRR_EMULATE_PREPARES => false
+                ];
+
+            $pdo = new PDO($conexion, $this->username, $this->password, $options);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo 'Error conexion: ' . $e;
+            exit;
+        }
+    }
+
 ?>
