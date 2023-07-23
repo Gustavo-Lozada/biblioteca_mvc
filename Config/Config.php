0a13814690a8cbd534560f51d0c3cb5dffa9071b
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-22 22:50:07 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
     const BASE_URL = "http://localhost/biblioteca_mvc/";
-    const HOST = "localhost";
-    const BD = "biblioteca_mvc";
-    const DB_USER = "root";
-    const PASS = "";
+    const HOST = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
+    const BD = "bpcek3ew2gqv7rdmv8nj";
+    const DB_USER = "useylzzrcbw6nak3";
+    const PASS = "6VILXUa95VDiqCGWTpom";
     const CHARSET = "charset=utf8";
 ?>
