4a47408a6c0ca27793804f00666baac162b52238
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-22 23:24:02 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,10 +1,10 @@
 <?php
+    const HOST = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
+    const BD = "bpcek3ew2gqv7rdmv8nj";
+    const DB_USER = "useylzzrcbw6nak3";
+    const PASS = "6VILXUa95VDiqCGWTpom";
+    const CHARSET = "charset=utf8";
 
-private $hostname = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
-private $database = "bpcek3ew2gqv7rdmv8nj";
-private $username = "useylzzrcbw6nak3";
-private $password = "6VILXUa95VDiqCGWTpom";
-private $charset = "utf8";
 
   $bdd = nueva PDO(
     	"mysql:host=". getenv("bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com") . ";nombrebd=" . getenv("bpcek3ew2gqv7rdmv8nj"),
