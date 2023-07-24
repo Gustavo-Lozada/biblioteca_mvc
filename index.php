a56d296bdd8e22ae78e0360fd0b25b13b0511ae2
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-23 20:05:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,4 +21,25 @@ if (isset($arrUrl[2])) {
 }
 require_once("Libraries/Core/Autoload.php");
 require_once("Libraries/Core/Load.php");
+
+
+// Incluir la clase de conexión a la base de datos
+require_once 'DatabaseConnection.php';
+
+// Crear una instancia de la conexión a la base de datos
+$databaseConnection = new DatabaseConnection();
+
+// Obtener la conexión
+$conexion = $databaseConnection->getConnection();
+
+// Realizar una consulta SELECT
+$consulta = "SELECT * FROM tabla";
+$statement = $conexion->prepare($consulta);
+$statement->execute();
+
+// Recorrer los resultados
+while ($fila = $statement->fetch(PDO::FETCH_ASSOC)) {
+    // Hacer algo con los datos obtenidos
+    echo $fila['columna1'] . ' - ' . $fila['columna2'] . '<br>';
+}
 ?>
