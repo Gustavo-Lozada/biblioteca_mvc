a029e90b07c067c00b8003768b412cead9cb90d1
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-23 20:03:08 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,11 +1,4 @@
 <?php
-    const HOST = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
-    const BD = "bpcek3ew2gqv7rdmv8nj";
-    const DB_USER = "useylzzrcbw6nak3";
-    const PASS = "6VILXUa95VDiqCGWTpom";
-    const CHARSET = "charset=utf8";
-
-
 
 class DatabaseConnection {
     private $servername = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
