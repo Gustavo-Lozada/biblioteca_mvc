d897e1856b0d89d23b7ca407683fb9d32646e60a
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-23 20:30:16 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,9 +1,11 @@
 <?php
 
-const HOST = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
-    const BD = "bpcek3ew2gqv7rdmv8nj";
-    const DB_USER = "useylzzrcbw6nak3";
-    const PASS = "6VILXUa95VDiqCGWTpom";
-    const CHARSET = "charset=utf8";
+MYSQL_ADDON_DB="bpcek3ew2gqv7rdmv8nj";
+MYSQL_ADDON_HOST="bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
+MYSQL_ADDON_PASSWORD="6VILXUa95VDiqCGWTpom";
+MYSQL_ADDON_PORT="3306";
+MYSQL_ADDON_URI="mysql://useylzzrcbw6nak3:6VILXUa95VDiqCGWTpom@bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com:3306/bpcek3ew2gqv7rdmv8nj";
+MYSQL_ADDON_USER="useylzzrcbw6nak3";
+MYSQL_ADDON_VERSION="8.0";
 
 ?>
