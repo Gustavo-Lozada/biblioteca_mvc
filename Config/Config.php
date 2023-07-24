83197b65d37e9aabfa1da6f906931aaad2b0f5a6
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-23 20:45:40 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,5 @@
 <?php
-    const BASE_URL = "mysql://useylzzrcbw6nak3:6VILXUa95VDiqCGWTpom@bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com:3306/bpcek3ew2gqv7rdmv8nj";
+    const BASE_URL = "https://app-ce8ee13b-20cd-4d3a-a8e4-d873aee00b98.cleverapps.io/";
     const HOST = "bpcek3ew2gqv7rdmv8nj-mysql.services.clever-cloud.com";
     const BD = "bpcek3ew2gqv7rdmv8nj";
     const DB_USER = "useylzzrcbw6nak3";
